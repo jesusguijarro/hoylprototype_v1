b7e9e27459d73ecb0b958a8063d5ecd5c7a9594c
Jesús Guijarro <72225659+jesusguijarro@users.noreply.github.com>
2024-11-26 12:41:53 -0600

    :construction: bear animations.

--- Assets/Scripts/Mobs/Bear.cs
+++ Assets/Scripts/Mobs/Bear.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Bear : MonoBehaviour
+public class Bear : Interactable, IEnemy
 {
-    public ArmColliderDamage legColliderDamage;
+    public ArmColliderDamage headColliderDamage;
 
     public LayerMask aggroLayerMask;
     public float currentHealth;
@@ -46,5 +46,103 @@ public class Bear : MonoBehaviour
         //_healthbar.UpdateHealthBar(maxHealth, currentHealth);       
 
         enemyAnimator = GetComponentInChildren<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        withinAggroColliders = Physics.OverlapSphere(transform.position, 10, aggroLayerMask); // aggro radius
+        if (withinAggroColliders.Length > 0)
+        {
+            ChasePlayer(withinAggroColliders[0].GetComponent<Player>());
+        }
+    }
+
+    public void PerformAttack()
+    {
+        enemyAnimator.SetBool("isAttacking", true);
+        //Debug.Log("damage to player");
+        //player.TakeDamage(5);
+        if (headColliderDamage != null)
+        {
+            Debug.Log("PerformAttack");
+            // Activate damage only during this attack
+            headColliderDamage.PerformAttack(10);
+            //playerAnimator.SetTrigger("Attack"); // Trigger attack animation
+
+            // Reset damage after animation duration (adjust timing to animation length)
+            StartCoroutine(ResetAttackActive(0.5f));
+        }
+        else Debug.Log("legColliderDamage is null");
+    }
+
+    IEnumerator ResetAttackActive(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        enemyAnimator.SetBool("isAttacking", false);
+    }
+    public void TakeDamage(int amount)
+    {
+        Debug.Log("Took damage.");
+        currentHealth -= amount;
+        //_healthbar.UpdateHealthBar(maxHealth, currentHealth);
+        if (currentHealth <= 0)
+            Die();
+    }
+    void ChasePlayer(Player player)
+    {
+        //enemyAnimator.Play("Roar");
+        //wait after Roar ends
+        this.player = player;
+        float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+
+        // Update destination only if distance is larger than stopping distance
+        if (distanceToPlayer > navAgent.stoppingDistance)
+        {
+            navAgent.SetDestination(player.transform.position);
+            enemyAnimator.SetBool("isMoving", true);  // Start running animation
+        }
+        else
+        {
+            enemyAnimator.SetBool("isMoving", false); // Stop running animation
+            if (!IsInvoking("PerformAttack"))
+            {
+                InvokeRepeating("PerformAttack", 0.5f, 2f);
+            }
+        }
+    }
+    public void Die()
+    {
+        //enemyAnimator.Play("Die");
+        navAgent.isStopped = true;
+        CombatEvents.EnemyDied(this);
+        Spawner.Respawn();
+        Destroy(gameObject);
+        //StartCoroutine(DestroyAfterAnimation());
+    }
+    //private IEnumerator DestroyAfterAnimation()
+    //{
+    //    //DropLoot();
+    //    // Wait until the animator is in the "Die" state to ensure the animation starts
+    //    while (!enemyAnimator.GetCurrentAnimatorStateInfo(0).IsName("Die"))
+    //    {
+    //        yield return null; // Wait until the Die state is active
+    //    }
+
+    //    // Wait for the full duration of the Die animation
+    //    yield return new WaitForSeconds(enemyAnimator.GetCurrentAnimatorStateInfo(0).length);
+
+    //    // Now destroy the object after the animation finishes
+    //    CombatEvents.EnemyDied(this);
+    //    Spawner.Respawn();
+    //    Destroy(gameObject);
+    //}
+    void DropLoot()
+    {
+        Item item = Droptable.GetDrop();
+        if (item != null)
+        {
+            PickupItem instance = Instantiate(pickupItem, transform.position, Quaternion.identity);
+            instance.ItemDrop = item;
+        }
     }
 }
