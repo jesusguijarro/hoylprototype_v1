abb6d1093a419dc17feaba1a0474aafb05e03624
Jesús Guijarro <guijarro497@gmail.com>
2024-12-08 23:01:26 -0600

    :adhesive_bandage: healthbar direction fix.

--- Assets/Scripts/Healthbar.cs
+++ Assets/Scripts/Healthbar.cs
@@ -6,9 +6,23 @@ using UnityEngine.UI;
 public class Healthbar : MonoBehaviour
 {
     [SerializeField] private Image _healthbarSprite;
+    [SerializeField] private float _reduceSpeed = 2;
+    private float _target = 1;
+    private Camera _cam;
+
+    private void Start()
+    {
+        _cam = Camera.main;        
+    }
 
     public void UpdateHealthBar(float maxHealth, float currentHealth)
     {
-        _healthbarSprite.fillAmount = currentHealth / maxHealth;
+        _target = currentHealth / maxHealth;
+    }
+
+    public void Update()
+    {
+        transform.rotation = Quaternion.LookRotation(transform.position - _cam.transform.position);
+        _healthbarSprite.fillAmount = Mathf.MoveTowards(_healthbarSprite.fillAmount, _target, _reduceSpeed * Time.deltaTime);
     }
 }
