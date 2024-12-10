aefce61d289f14efd38159616e26ea30375837e1
Omar Montoya <omarmontoyaromero@gmail.com>
2024-12-09 20:20:12 -0600

    👍 se agrego el revivir en la penultima posicion agregada en el prefab

--- Assets/Scripts/SavePlayerPosition.cs
+++ Assets/Scripts/SavePlayerPosition.cs
@@ -1,19 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SavePlayerPosition : MonoBehaviour
 {
     private Player player;
-    Vector3 playerPosition;
+    private int positionIndex = 0; // Índice para el seguimiento de las posiciones
+    private float saveInterval = 5f; // Intervalo de tiempo para guardar la posición
+    private float lastSaveTime = 0f;
 
-    private void Start()
+    void Start()
     {
-        player = FindAnyObjectByType<Player>();  // Ensure player is correctly assigned
+        player = FindAnyObjectByType<Player>(); // Asegúrate de que el jugador está correctamente asignado
 
         if (PlayerPrefs.HasKey("playerStarted"))
         {
-            LoadPlayerPosition();
+            LoadPlayerPosition(); // Cargar la última posición guardada
         }
 
         if (!PlayerPrefs.HasKey("playerStarted"))
@@ -23,37 +23,61 @@ public class SavePlayerPosition : MonoBehaviour
         }
     }
 
+    void Update()
+    {
+        // Guardar la posición cada cierto tiempo
+        if (Time.time - lastSaveTime >= saveInterval)
+        {
+            SavePosition();
+            lastSaveTime = Time.time;
+        }
+    }
+
     public void SavePosition()
     {
-        playerPosition = player.transform.position;
-        
-        PlayerPrefs.SetFloat("playerPositionX", playerPosition.x);
-        PlayerPrefs.SetFloat("playerPositionY", playerPosition.y);
-        PlayerPrefs.SetFloat("playerPositionZ", playerPosition.z);
+        Vector3 playerPosition = player.transform.position;
+
+        // Guardar la posición con un índice incremental
+        PlayerPrefs.SetFloat("playerPosition_" + positionIndex + "_X", playerPosition.x);
+        PlayerPrefs.SetFloat("playerPosition_" + positionIndex + "_Y", playerPosition.y);
+        PlayerPrefs.SetFloat("playerPosition_" + positionIndex + "_Z", playerPosition.z);
         PlayerPrefs.Save();
 
-        Debug.Log("Player position saved!");
+        Debug.Log("Player position saved at index: " + positionIndex);
+
+        // Incrementar el índice para la próxima posición guardada
+        positionIndex++;
     }
-    
-    public void ResetPlayerPosition() // only use this for develop porpuse
+
+    public void ResetPlayerPosition()
     {
         PlayerPrefs.DeleteAll();
         PlayerPrefs.Save();
-
         Debug.Log("PlayerPrefs reset!");
     }
 
-    private void LoadPlayerPosition()
+    public void LoadPlayerPosition()
     {
         player.gameObject.SetActive(false);
-        Vector3 savedPosition = new Vector3(
-            PlayerPrefs.GetFloat("playerPositionX"),
-            PlayerPrefs.GetFloat("playerPositionY"),
-            PlayerPrefs.GetFloat("playerPositionZ")
-        );
-        player.transform.position = savedPosition;
-        player.gameObject.SetActive(true);
 
-        Debug.Log("Player position loaded!");
+        // Buscar la penúltima posición guardada
+        int targetIndex = positionIndex - 2; // Índice de la penúltima posición guardada
+
+        if (targetIndex >= 0) // Asegúrate de que el índice es válido
+        {
+            Vector3 savedPosition = new Vector3(
+                PlayerPrefs.GetFloat("playerPosition_" + targetIndex + "_X"),
+                PlayerPrefs.GetFloat("playerPosition_" + targetIndex + "_Y"),
+                PlayerPrefs.GetFloat("playerPosition_" + targetIndex + "_Z")
+            );
+            player.transform.position = savedPosition;
+            Debug.Log("Player position loaded at index: " + targetIndex);
+        }
+        else
+        {
+            Debug.LogError("No saved position found for the specified index.");
+        }
+
+        player.gameObject.SetActive(true);
     }
 }
