a79568a7c774cd43c51dd02a791d8f8c8303e0cd
Jesús Guijarro <guijarro497@gmail.com>
2025-01-02 21:46:12 -0600

    🚧 scene management after register.

---
+++ Assets/Scripts/ScenesManager/SceneController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("PlayerUsername"))
+        {
+            // player already register
+            SceneManager.LoadScene("SampleScene");
+        }
+        else
+        {
+            Debug.Log("Player not registered. Staying on main menu.");
+        }
+    }
+}
